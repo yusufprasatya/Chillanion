c23de4abae2cec8bce4e8ad92f0492335a18254f
yusufprasatya <yusufmarsudien10@gmail.com>
2023-10-10 14:25:08 +0700

    Initial Commit

---
+++ SleepWell/SleepWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepWellApp.swift
+//  SleepWell
+//
+//  Created by Muhammad Yusuf on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepWell/View/Screen/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenView.swift
+//  SleepWell
+//
+//  Created by Muhammad Yusuf on 10/10/23.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
